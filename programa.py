466d0726485dad36d10d9eb1d033f5b6ddf31a27
Gabriel Mendivil <mendivilg@gmail.com>
2019-11-21 16:09:13 -0300

    sigo intentando con las graficas...

--- programa.py
+++ programa.py
@@ -19,15 +19,15 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         # Genero grafica con Matplotlib
-        grafica_bode = FigureCanvas(Figure(figsize=(5, 3)))
-        self.ui.verticalLayout_nyquist.addStretch()
+
+        grafica_bode = FigureCanvas(Figure())
         self.ui.verticalLayout_nyquist.addWidget(grafica_bode)
-        self.addToolBar(NavigationToolbar(grafica_bode, self))
+        #self.addToolBar(NavigationToolbar(grafica_bode, self))
         self._bode_ax = grafica_bode.figure.subplots()
 
-        grafica_nyquist = FigureCanvas(Figure(figsize=(5, 3)))
+        grafica_nyquist = FigureCanvas(Figure())
         self.ui.verticalLayout_bode.addWidget(grafica_nyquist)
-        self.addToolBar(NavigationToolbar(grafica_nyquist, self))
+        #self.addToolBar(NavigationToolbar(grafica_nyquist, self))
         self._nyquist_ax = grafica_nyquist.figure.subplots()
 
         t = np.linspace(0, 10, 501)
